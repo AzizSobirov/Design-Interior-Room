1c4f8cffc1ab22d95c154e16823e367aaf0d3f81
Aziz Sobirov <azizsobirovhazarasp@gmail.com>
2023-05-14 18:02:53 +0500

    14.05.2023

---
+++ js/app.js
@@ -0,0 +1,62 @@
+[].forEach.call(document.querySelectorAll("#number"), function (input) {
+  let keyCode;
+  function mask(event) {
+    event.keyCode && (keyCode = event.keyCode);
+    let pos = this.selectionStart;
+    if (pos < 3) event.preventDefault();
+    let matrix = "+7 (___) ___-__-__",
+      i = 0,
+      def = matrix.replace(/\D/g, ""),
+      val = this.value.replace(/\D/g, ""),
+      newValue = matrix.replace(/[_\d]/g, function (a) {
+        return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+      });
+    i = newValue.indexOf("_");
+    if (i != -1) {
+      i < 5 && (i = 3);
+      newValue = newValue.slice(0, i);
+    }
+    let reg = matrix
+      .substr(0, this.value.length)
+      .replace(/_+/g, function (a) {
+        return "\\d{1," + a.length + "}";
+      })
+      .replace(/[+()]/g, "\\$&");
+    reg = new RegExp("^" + reg + "$");
+    if (
+      !reg.test(this.value) ||
+      this.value.length < 5 ||
+      (keyCode > 47 && keyCode < 58)
+    )
+      this.value = newValue;
+    if (event.type == "blur" && this.value.length < 5) this.value = "";
+  }
+
+  input.addEventListener("input", mask, false);
+  input.addEventListener("focus", mask, false);
+  input.addEventListener("blur", mask, false);
+  input.addEventListener("keydown", mask, false);
+});
+
+let nav = document.querySelector(".nav");
+window.addEventListener("scroll", function () {
+  nav.classList.toggle("sticky", window.scrollY > 0);
+});
+
+function openForm() {
+  document.querySelector(".popup").style.display = "flex";
+}
+
+function submitForm() {
+  document.querySelector(".popup-form").style.display = "none";
+  document.querySelector(".popup-success").style.display = "flex";
+  setTimeout(() => {
+    closePopup();
+  }, 3000);
+}
+
+function closePopup() {
+  document.querySelector(".popup-form").style.display = "block";
+  document.querySelector(".popup-success").style.display = "none";
+  document.querySelector(".popup").style.display = "none";
+}
